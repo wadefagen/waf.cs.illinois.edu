ad9ec0e1f42fa8e88b449f675609fb4acb29e383
Wade Fagen-Ulmschneider <waf@illinois.edu>
2020-11-02 09:18:56 -0600

    daily COVID-19 data update

--- covid-19/src/updated.js
+++ covid-19/src/updated.js
@@ -1,4 +1,4 @@
-_dateUpdated = "11/01/2020";
-_reqStr = "2020-11-01";
+_dateUpdated = "11/02/2020";
+_reqStr = "2020-11-02";
 
-$("#jhu-updated").html("(Updated: 11/01 11:52am CT)");
+$("#jhu-updated").html("(Updated: 11/02 09:18am CT)");
