49914ae38a62b407de3e48469a577dfa70f61c27
Wade Fagen-Ulmschneider <waf@illinois.edu>
2021-09-09 16:10:19 -0500

    dategen TR

--- collections/_tools/dategen.js
+++ collections/_tools/dategen.js
@@ -1,9 +1,13 @@
-var date = new Date("2020-08-25");
-var endDate = new Date("2020-12-10");
+
+var date = new Date("2021-08-24");
+var endDate = new Date("2021-12-08");
 let s;
 var i = 1;
 
 while (date < endDate) {
+
+
+
   s = date.toISOString().substring(0, 10);
   console.log(`# Week ${i}`);
   console.log(`- date: ${s}`);
